5915492cbe9c2c89eb953b446c9bf5a2ce4d6d11
osmnishida <osmnishida@gmail.com>
2023-10-15 20:03:31 +0900

    取引タイプ毎にデータ取得

--- app/Http/Controllers/TransactionCaseSearchController.php
+++ app/Http/Controllers/TransactionCaseSearchController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class TransactionCaseSearchController extends Controller
 {
     public function tcsearch(Request $request) {
+        // phpinfo();
         $fromyyyyn = $request->fromyyyyn;
         $toyyyyn = $request->toyyyyn;
         $citycode = $request->citycode;
@@ -15,17 +16,24 @@ class TransactionCaseSearchController extends Controller
         // return $citycode;
         // return view('tcsearch', compact('fromyyyyn'));
 
-        $basicTradeSearch = "https://www.land.mlit.go.jp/webland/api/TradeListSearch?";
+        $basicTradeSearch = 'https://www.land.mlit.go.jp/webland/api/TradeListSearch?';
         $tradeSearchUrl = "$basicTradeSearch" . "from=" . "$fromyyyyn" . "&to=" . "$toyyyyn" . "&city=" . "$citycode";
         // return $tradeSearchUrl;
-        $jsonTradeSearch = file_get_contents($tradeSearchUrl);
+        // $jsonTradeSearch = file_get_contents($tradeSearchUrl);
+        // $jsonTradeSearch = readfile($tradeSearchUrl);
+        // readfile($tradeSearchUrl);
         // return $jsonTradeSearch;
-        $arrTradeSearch = json_decode($jsonTradeSearch,true);
+
+        $resp = \Http::get($tradeSearchUrl);
+        $arrResp = json_decode($resp->body(),true);
+        // return $arrResp;
+
+        // $arrTradeSearch = json_decode($jsonTradeSearch,true);
         // return $arrTradeSearch;
-        // dd($arrTradeSearch);
-        $arrTradeSearch = $arrTradeSearch["data"];
-        // return $arrTradeSearch;
-        return view('searchresults', compact('arrTradeSearch'));
+        // dd($arrResp);
+        $arrResp = $arrResp["data"];
+        // return $arrResp;
+        return view('searchresults', compact('arrResp'));
     
     }
 }
--- resources/views/searchresults.blade.php
+++ resources/views/searchresults.blade.php
@@ -7,18 +7,58 @@
   <body>
     <h1>取引事例検索結果</h1>
 
-      @foreach ($arrTradeSearch as $tradeSearch)
+      @foreach ($arrResp as $resp)
     <?php 
     // print_r($arrjsoncc);
     // echo $cc[0];
     ?>
-    <p>取引の種類:{{ $tradeSearch["Type"] }}　　　地区:{{ $tradeSearch["Region"] }}　　　市区町村名:{{ $tradeSearch["Municipality"] }}
-    　　　地区名:{{ $tradeSearch["DistrictName"] }}　　　取引価格（総額）：{{ $tradeSearch["TradePrice"] }}　　　面積（㎡）:{{ $tradeSearch["Area"] }}</p>
-  
+        @switch($resp["Type"])
+          @case("宅地(土地)")
+            <p>取引の種類:{{ $resp["Type"] }}　　　地区:{{ $resp["Region"] }}　　　都道府県名:{{ $resp["Prefecture"] }}　　　
+              市区町村名:{{ $resp["Municipality"] }}　　　地区名:{{ $resp["DistrictName"] }}　　　
+              :{{ $resp["Type"] }}　　　取引価格（総額）：{{ $resp["TradePrice"] }}　　　坪単価:{{ $resp["PricePerUnit"] }}　　　
+              面積（㎡）:{{ $resp["Area"] }}　　　取引価格（㎡単価）:{{ $resp["UnitPrice"] }}　　　土地の形状:{{ $resp["LandShape"] }}　　　
+              全面道路・方位:{{ $resp["Direction"] }}　　　
+              都市計画:{{ $resp["CityPlanning"] }}　　　建ぺい率:{{ $resp["CoverageRatio"] }}　　　容積率:{{ $resp["FloorAreaRatio"] }}　　　取引時点:{{ $resp["Period"] }}　　　</p>
+            @break
+        @endswitch
+      @endforeach
+      @foreach ($arrResp as $resp)
+        @switch($resp["Type"])
+          @case("宅地(土地と建物)")
+            <p>取引の種類:{{ $resp["Type"] }}　　　市区町村名:{{ $resp["Municipality"] }}
+          　　　地区名:{{ $resp["DistrictName"] }}　　　取引価格（総額）：{{ $resp["TradePrice"] }}　　　面積（㎡）:{{ $resp["Area"] }}</p>
+            @break
+        @endswitch
+      @endforeach
+      @foreach ($arrResp as $resp)
+        @switch($resp["Type"])
+          @case("中古マンション等")
+            <p>取引の種類:{{ $resp["Type"] }}　　　市区町村名:{{ $resp["Municipality"] }}
+            　　　地区名:{{ $resp["DistrictName"] }}　　　取引価格（総額）：{{ $resp["TradePrice"] }}　　　面積（㎡）:{{ $resp["Area"] }}</p>
+            @break
+        @endswitch
+      @endforeach
+      @foreach ($arrResp as $resp)
+        @switch($resp["Type"])
+          @case("農地")
+          <p>取引の種類:{{ $resp["Type"] }}　　　市区町村名:{{ $resp["Municipality"] }}
+            　　　地区名:{{ $resp["DistrictName"] }}　　　取引価格（総額）：{{ $resp["TradePrice"] }}　　　面積（㎡）:{{ $resp["Area"] }}</p>
+            @break
+        @endswitch
+      @endforeach
+      @foreach ($arrResp as $resp)
+        @switch($resp["Type"])
+          @case("林地")
+          <p>取引の種類:{{ $resp["Type"] }}　　　市区町村名:{{ $resp["Municipality"] }}
+            　　　地区名:{{ $resp["DistrictName"] }}　　　取引価格（総額）：{{ $resp["TradePrice"] }}　　　面積（㎡）:{{ $resp["Area"] }}</p>
+            @break
+        @endswitch
+      @endforeach
+
     <?php
     // print_r($cc);
     ?>
-      @endforeach
 
     {{--
     {{$arrjsoncc}}[data][0][id];
